aacf04a0d8097f0ddcf0b85ea4d21a5acebf794f
Pedro Paulo <pedropaulop10035@gmail.com>
2026-04-17 15:36:44 -0300

    atualizando vite.config.js e pequeno ajuste em franquie.html

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import {defineConfig} from 'vite'
+import {resolve} from 'path'
 
 export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				main: './home.html',
+				main: resolve(__dirname, './home.html'),
+				'quem-somos': resolve(__dirname, './quem-somos.html'),
+				'entre-contato': resolve(__dirname, './entre-contato.html'),
+				franquie: resolve(__dirname, './franquie.html'),
+				creditos: resolve(__dirname, './creditos.html'),
 			},
 		},
 	},
