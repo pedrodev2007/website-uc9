c478d21141897e10902acc3ae988918ac9e82fb8
PEDRO PAULO PEREIRA DE OLIVEIRA <pedro.ppoliveira1@senacsp.edu.br>
2026-06-03 16:54:35 -0300

    configurando tela no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, './home.html'),
+				login: resolve(__dirname, './login.html'),
 				'quem-somos': resolve(__dirname, './quem-somos.html'),
 				'entre-contato': resolve(__dirname, './entre-contato.html'),
 				franquie: resolve(__dirname, './franquie.html'),
