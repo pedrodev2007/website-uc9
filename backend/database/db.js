33e3a0a4216c0d123f5841768ee6756406beb89c
Pedro Paulo <pedropaulop10035@gmail.com>
2026-06-25 15:12:54 -0300

    Add port configuration to database connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
+    port: process.env.PORT,
     password: process.env.DB_PASSWORD || '', 
     database: process.env.DB_NAME || 'lavanderia',
     waitForConnections: true,
