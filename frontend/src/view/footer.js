d7e730aa05b6666527f0c0223f167ffa75cbd523
Pedro Paulo <pedropaulop10035@gmail.com>
2026-06-26 08:56:39 -0300

    Update footer links to use absolute paths

--- frontend/src/view/footer.js
+++ frontend/src/view/footer.js
@@ -5,18 +5,18 @@ if (FOOTER.classList.contains('main-footer')) {
     
     <div id="footer-top-container">
 			<div class="footer-top-div">
-				<a href="pages/quem-somos/index.html">QUEM SOMOS</a>
-				<a href="">NOSSAS LAVANDERIAS</a>
+				<a href="/quem-somos/">QUEM SOMOS</a>
+				<a href="/nossas-lavanderias/">NOSSAS LAVANDERIAS</a>
 			</div>
 			<div class="footer-top-div">
-				<a href="pages/entre-contato/index.html">ENTRE EM CONTATO</a>
-				<a href="pages/franquie/index.html">FRANQUIE</a>
+				<a href="/entre-contato/">ENTRE EM CONTATO</a>
+				<a href="/franquie/">FRANQUIE</a>
 			</div>
 		</div>
 		<div id="footer-bottom-container">
 			<div class="footer-bottom-div">
 				<p>Atendimento 24 horas, de domingo a domingo.</p>
-				<a href="pages/creditos/index.html" id="credits">Créditos</a>
+				<a href="/creditos/" id="credits">Créditos</a>
 			</div>
 		</div>	
     
